ea89257d068a74766e1f905567498c32f8bf656e
CloudDeLuna <herbaut.robin@gmail.com>
2015-11-13 16:40:20 +0100

    Rendu TP5 Robin Herbaut

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include "trianglewindow.h"
+#include "gameserver.h"
+#include "gameclient.h"
 
 #include <QtGui/QGuiApplication>
 #include <QtGui/QMatrix4x4>
@@ -15,49 +16,78 @@
 #include <QtCore>
 #include <QtGui>
 
-#include <omp.h>
+#include "thread.h"
 
 using namespace std;
 
-
+/************************************************/
 int main(int argc, char **argv)
 {
-//test omp
-//#pragma omp parallel
-//    qDebug() << "Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads();
-
     srand(time(NULL));
     QGuiApplication app(argc, argv);
-    
+
     QSurfaceFormat format;
     format.setSamples(16);
-    
-    paramCamera* c=new paramCamera();
-    
-    QTimer* calendar = new QTimer;
 
-    TriangleWindow* window[4];
-    for(int i = 0; i < 4; i++)
-    {
-        if (i == 0)
-            window[i] = new TriangleWindow();
-        else
-            window[i] = new TriangleWindow(30);
-        window[i]->setSeason(i);
-        window[i]->c = c;
-        window[i]->setFormat(format);
-        window[i]->resize(500,375);
-        int x = i%2;
-        int y = i>>1;
-                
-        window[i]->setPosition(x*500,y*450);
-        window[i]->show();
+    gamecamera* camera = new gamecamera();
+    camera->m_temps = RIEN;
+    /**********Partie 1****************** /
+    GameServer window (120, camera);
+    window.setFormat(format);
+    window.setTitle("Serveur");
+    window.resize(320, 240);
+    window.show();
+    window.setAnimating(true);
 
-        calendar->connect(calendar, SIGNAL(timeout()),window[i], SLOT(updateSeason()));
-    }
-    
-    calendar->start(20);
+    thread client(camera, "client 1");
+    client.start();
+
+    thread client2(camera, "client 2");
+    client2.start();
+
+    thread client3(camera, "client 3");
+    client3.start();
+
+    thread client4(camera, "client 4");
+    client4.start();
+
+    /*************Partie 2***************/
+    GameWindow window1 (200, camera, PRINTEMPS);
+    window1.setFormat(format);
+    window1.setTitle("Printemps");
+    window1.resize(640, 480);
+    window1.setPosition(0, 0);
+    window1.show();
+    window1.setAnimating(true);
+    /* <--- Supprimer cette ligne et commenter la partie 1 pour tester la partie 2* /
+    GameWindow window2 (120, camera, ETE);
+    window2.setFormat(format);
+    window2.setTitle("Ete");
+    window2.resize(640, 480);
+    window2.setPosition(0, 0);
+    window2.show();
+    window2.setAnimating(true);
+
+    GameWindow window3 (120, camera, AUTOMNE);
+    window3.setFormat(format);
+    window3.setTitle("Automne");
+    window3.resize(640, 480);
+    window3.setPosition(0, 250);
+    window3.show();
+    window3.setAnimating(true);
+
+    GameWindow window4 (120, camera, HIVER);
+    window4.setFormat(format);
+    window4.setTitle("Hiver");
+    window4.resize(680, 480);
+    window2.setPosition(330, 0);
+    window4.show();
+    window4.setAnimating(true);
+
+    /********************************************/
 
     return app.exec();
 }
+/***************************************/
+
 
